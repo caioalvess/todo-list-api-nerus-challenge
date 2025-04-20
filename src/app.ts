be4bbd1069ee0af7376e6514e2e5744804d08bd1
caioalvess <caio_f.alves@hotmail.com>
2025-04-20 11:26:46 -0300

    feat: add CORS middleware to enable cross-origin requests

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,20 @@ import { todoRoutes } from "./routes/TodoRoutes";
 import { errorHandler } from "./middlewares/ErrorHandler";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./swagger";
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors());
+
 app.use(express.json());
+
+app.use(express.json());
+
 app.use("/api", todoRoutes);
+
 app.use(errorHandler);
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 export default app;
